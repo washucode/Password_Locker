bb821bef827eb39cd69a4e4ba853aa5c11caa3e4
washucode <estherw8525@gmail.com>
2019-09-02 04:29:51 +0300

    add function to create new user

--- runapp.py
+++ runapp.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3.6
 from usercredentials import users, Credentials
-def 
+def create_user(fname,sname,uname,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = users(fname,sname,uname,password)
+    return new_user
 
